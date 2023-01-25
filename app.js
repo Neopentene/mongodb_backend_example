d6e69d17c427a6b1262ffb434e8fde7ac0211a78
Neopentene <celestynkinny3@gmail.com>
2023-01-25 20:10:32 +0530

    initial commit

--- app.js
+++ app.js
@@ -11,56 +11,48 @@ dotenv.config();
 
 // Importing libraries
 import express from "express";
-import { MongoClient } from "mongodb";
 import cors from "cors";
 import engine from "ejs";
 
 import { usersRouter } from "./routes/users.js";
 import { tasksRouter } from "./routes/tasks.js";
+// Initialize server application
+const app = express();
 
-async function main() {
-  // Creating a connection to mongodb
-  const connection = new MongoClient(process.env.URL);
-  let instance = await connection.connect();
-  const app = express();
+// CORS configuration
+app.use(cors());
+app.use(
+  express.json({
+    type: [
+      "application/json",
+      "multipart/form-data",
+      "application/x-www-form-urlencoded",
+    ],
+  })
+);
 
-  // CORS configuration
-  app.use(cors());
-  app.use(
-    express.json({
-      type: [
-        "application/json",
-        "multipart/form-data",
-        "application/x-www-form-urlencoded",
-      ],
-    })
-  );
+// Setup for rendering html pages
+app.engine("html", engine.renderFile);
+app.set("views", "./public");
+app.set("view engine", "ejs");
 
-  // Setup for rendering html pages
-  app.engine("html", engine.renderFile);
-  app.set("views", "./public");
-  app.set("view engine", "ejs");
+// Route to serve application
+app.get("/", async (req, res) => {
+  // TODO: Replace with your own html
+  res.render("index.html");
+});
 
-  // Route to serve application
-  app.get("/", async (req, res) => {
-    // TODO: Replace with your own html
-    res.render("index.html");
-  });
+app.use("/user", usersRouter);
+app.use("/task", tasksRouter);
 
-  app.use("/user", usersRouter);
-  app.use("/task", tasksRouter);
-
-  const server = app.listen(
-    Number.parseInt(process.env.PORT),
-    process.env.ADDRESS,
-    () => {
-      console.log(
-        "Sever listening on http://%s:%s",
-        process.env.ADDRESS,
-        process.env.PORT
-      );
-    }
-  );
-}
-
-main();
+const server = app.listen(
+  Number.parseInt(process.env.PORT),
+  process.env.ADDRESS,
+  () => {
+    console.log(
+      "Sever listening on http://%s:%s",
+      process.env.ADDRESS,
+      process.env.PORT
+    );
+  }
+);
